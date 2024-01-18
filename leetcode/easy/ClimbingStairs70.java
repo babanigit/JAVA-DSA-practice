25ff3881ac0db2f0579a013ef8c05db1427beb72
bablu <aniketpanchal07@gmail.com>
2024-01-18 14:27:36 +0530

    new files added

---
+++ leetcode/easy/ClimbingStairs70.java
@@ -0,0 +1,24 @@
+package easy;
+public class ClimbingStairs70 {
+
+    public static int ClimbingStairs(int n) {
+
+        if (n == 1)
+            return 1;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+
+        int n = 7;
+        System.out.println(ClimbingStairs(n));
+
+    }
+}
