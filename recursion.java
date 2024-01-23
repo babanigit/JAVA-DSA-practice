8699cc37a0bb33d6ad0412809363ecc1206cf79c
bablu <aniketpanchal07@gmail.com>
2024-01-23 17:31:43 +0530

    some files moving

---
+++ recursion.java
@@ -0,0 +1,18 @@
+public class recursion {
+    
+
+    public static int sum(int k) {
+        if (k > 0) {
+            System.out.println(k);
+          return k + sum(k - 1);
+        } else {
+          return 0;
+        }
+      }
+
+
+    public static void main(String[] args) {
+        int result = sum(10);
+        System.out.println(result);
+      }
+}
