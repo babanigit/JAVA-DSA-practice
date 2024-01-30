9a0bcbc392156c318ea17331a446cadef4b7c393
bablu <aniketpanchal07@gmail.com>
2024-01-31 00:00:32 +0530

    new files

---
+++ EvaluateReversePolishNotation150.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+
+public class EvaluateReversePolishNotation150 {
+
+    public static int evalRPN(String[] token) {
+
+        // creating Stack Object
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for (String store : token) {
+            if ("+".equals(store))
+                stack.push(stack.pop() + stack.pop());
+
+            else if ("-".equals(store))
+                stack.push(-(stack.pop() - stack.pop()));
+
+            else if ("*".equals(store))
+                stack.push((stack.pop() * stack.pop()));
+
+            else if ("/".equals(store)) {
+                int y = stack.pop();
+                int x = stack.pop();
+                stack.push(x / y);
+                System.out.println(stack);
+            }
+
+            else
+                stack.push(Integer.valueOf(store));
+        }
+
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+
+        String[] strs = new String[] {
+                // "2", "1", "+", "3", "*"
+                "4","13","5","/","+"
+        };
+
+        System.out.println(evalRPN(strs));
+    }
+
+}
