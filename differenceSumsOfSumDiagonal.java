932a2b0686e9a4e3c53f6fc704d13f5ad69e469f
bablu <aniketpanchal07@gmail.com>
2024-02-04 13:51:11 +0530

    added

--- differenceSumsOfSumDiagonal.java
+++ differenceSumsOfSumDiagonal.java
@@ -1,46 +1,65 @@
+
 // JAVA Code for Find difference between sums
 // of two diagonals
 import java.io.*;
+
 class differenceSumsOfSumDiagonal {
-	
-	public static int difference(int arr[][], int n)
-	{
+
+	public static int difference(int arr[][]) {
 		// Initialize sums of diagonals
 		int d1 = 0, d2 = 0;
-	
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				// finding sum of primary diagonal
-				if (i == j)
+		int n = arr.length;
+
+		// rows 
+		for (int i = 0; i < n; i++) {
+			// columns 
+			for (int j = 0; j < n; j++) {
+				// finding sum of primary diagonal, from left to right \
+				if (i == j) {
 					d1 += arr[i][j];
-	
-				// finding sum of secondary diagonal
-				if (i == n - j - 1)
+					System.out.println(arr[i][j]);
+				}
+				// finding sum of secondary diagonal, from right to left /
+				if (i == n - j - 1) {
 					d2 += arr[i][j];
+				}
 			}
 		}
-	
+		// System.out.println(d1);
+
+		// // rows 
+		// for (int i = 0; i < n; i++) { //int j = 0; j < n; j++
+		// 	// columns 
+		// 	for (int j = 2; j >= 0; j--) {//int i = 2; i >= 0; i--
+		// 		// finding sum of primary diagonal, from left to right \
+		// 		if (i == j) {
+		// 			d2 += arr[i][j];
+		// 			System.out.println(arr[i][j]);
+		// 		}
+		// 		// finding sum of secondary diagonal, from right to left /
+		// 		// if (i == n - j - 1)
+		// 		// System.out.println(n-j-1);
+		// 		// 	d2 += arr[i][j];
+		// 	}
+		// }
+		// System.out.println(d2);
+
 		// Absolute difference of the sums
 		// across the diagonals
 		return Math.abs(d1 - d2);
 	}
-	
+
 	/* Driver program to test above function */
-	public static void main(String[] args) 
-	{
-		int n = 3;
-		
-		int arr[][] =
-		{
-			{11, 2, 4},
-			{4 , 5, 6},
-			{10, 8, -12}
+	public static void main(String[] args) {
+
+		int arr[][] = {
+				{ 11, 2, 4 },
+				{ 4, 5, 6 },
+				{ 10, 8, -12 }
 		};
-	
-		System.out.print(difference(arr, n));
-		
+
+		System.out.print(difference(arr));
+
 	}
 }
 // This code is contributed by Arnav Kr. Mandal.
