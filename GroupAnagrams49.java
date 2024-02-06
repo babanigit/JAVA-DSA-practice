f42e42b2f59828cb53a2d671c46502c827e26625
bablu <aniketpanchal07@gmail.com>
2024-02-06 20:48:32 +0530

    new files added

---
+++ GroupAnagrams49.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class GroupAnagrams49 {
+    public static List<List<String>> groupAnagrams(String[] strs){
+
+        List<List<String>> list = new ArrayList<>();
+        HashMap<String,List<String>> map = new HashMap<>();
+        System.out.println(strs.length);
+
+        for (int i = 0; i < strs.length; i++) {
+
+            // String chars = strs[i];  
+            char[] chars = strs[i].toCharArray();
+            System.out.println(chars);
+            Arrays.sort(chars); //for sorting you chat use string
+            String sortedStr = new String(chars);
+            
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+            map.get(sortedStr).add(strs[i]);
+        }
+
+        for(List<String> data : map.values()) {
+            list.add(data);
+        }
+
+        return list;
+    }
+
+    
+
+    public static void main(String[] args) {
+        
+        String[] strs = new String[]{
+            "eat","tea","tan","ate","nat","bat"
+        };
+
+        System.out.println(groupAnagrams(strs));
+
+    }
+}
+
+// List<List<String>> 
+// list =
+// [["bat"],
+// ["nat","tan"],
+// ["ate","eat","tea"]]
