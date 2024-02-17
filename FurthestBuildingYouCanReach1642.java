9c680b3743c64ab468d5caa34e4bc3f7e8bab897
bablu <aniketpanchal07@gmail.com>
2024-02-17 15:57:38 +0530

    new files added

---
+++ FurthestBuildingYouCanReach1642.java
@@ -0,0 +1,33 @@
+import java.util.PriorityQueue;
+
+public class FurthestBuildingYouCanReach1642 {
+
+    public static int furthestBuilding(int[] heights, int bricks, int ladders) {
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+        for (int i = 0; i < heights.length-1; i++) {
+            int diff = heights[i+1] - heights[i];
+
+            if (diff>0) {
+                heap.offer(diff);   
+
+                if (heap.size() > ladders) {
+                    bricks -= heap.poll();
+                }
+                if (bricks< 0) {
+                    return i;
+                }
+            }
+        }
+        return heights.length - 1 ;
+    }
+    
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {
+            4,2,7,6,9,14,12
+        };
+        System.out.println(furthestBuilding(arr, 5, 1));
+    }
+}
