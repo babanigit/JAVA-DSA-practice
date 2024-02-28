6d1d3ed0f5abc9d22705ad91fa4507e3aa491b26
bablu <aniketpanchal07@gmail.com>
2024-02-28 11:43:30 +0530

    added medium sum

--- leetcode/medium/ContainerWithMostWater11.java
+++ leetcode/medium/ContainerWithMostWater11.java
@@ -7,10 +7,13 @@ public class ContainerWithMostWater11 {
         int maxArea=0;
 
         while (left<right) { //0 8, 1 8, ...
+
             int currentArea= Math.min(height[left],height[right]) * (right-left); //1*8=8 ,7*7=49 , ...
-            // System.out.println("the currentArea is : "+ currentArea);
+            System.out.println("the currentArea is : "+ currentArea);
+            System.err.println(right-left);
+
             maxArea = Math.max(maxArea, currentArea); //8 , 49 , ...
-            // System.out.println("the maxArea is : "+ maxArea);
+            System.out.println("the maxArea is : "+ maxArea);
 
             // increment or decrement
             if (height[left]<height[right]) {
